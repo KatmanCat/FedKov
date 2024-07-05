c02b735b3a7d6819cdf6196870035a2acc3da76b
Fedor Kovtunov <75466gd@gmail.com>
2024-07-05 17:50:59 +0300

    Практическое задание 2. Реализовал страницу Hello,

---
+++ src/test/java/ru/Fedor/tgBot/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Fedor.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
