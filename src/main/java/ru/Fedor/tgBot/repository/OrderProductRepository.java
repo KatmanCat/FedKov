a0d5d841c775ecef1ec3a9124f720b671e9fe3de
Fedor Kovtunov <75466gd@gmail.com>
2024-07-12 00:24:44 +0300

    Практическое задание 3. Исправление ошибок 3

--- src/main/java/ru/Fedor/tgBot/repository/OrderProductRepository.java
+++ src/main/java/ru/Fedor/tgBot/repository/OrderProductRepository.java
@@ -2,14 +2,14 @@ package ru.Fedor.tgBot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import ru.Fedor.tgBot.entity.Client;
+import ru.Fedor.tgBot.entity.OrderProduct;
 
 @RepositoryRestResource(
         collectionResourceRel = "orderProducts",
         path = "orderProducts"
 )
 
-public interface OrderProductRepository extends JpaRepository<Client, Long>
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long>
 {
 
 }
